58adcc7d2d428324149260193d08273a6d3f83ce
Ibo Sy <ibo@b9lab.com>
2024-04-03 04:03:42 +0200

    add ai integration

--- app/aiFunctions.ts
+++ app/aiFunctions.ts
@@ -18,9 +18,29 @@ export async function getAiRespone(
 }
 
 async function getGeminiAIResponse(prompt: string): Promise<string | null> {
-  return "not implemented";
+  const genAI = new GoogleGenerativeAI(
+    process.env["NEXT_PUBLIC_GEMINI_API_KEY"]!
+  );
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+  const result = await model.generateContent({
+    contents: [{ role: "user", parts: [{ text: prompt }] }],
+  });
+  const message = result.response.candidates?.at(0)?.content.parts.at(0)?.text;
+  return message ? message : null;
 }
 
 async function getOpenAIResponse(prompt: string): Promise<string | null> {
-  return "not implemented";
+  const openAI = new OpenAI({
+    apiKey: process.env["NEXT_PUBLIC_OPENAI_API_KEY"],
+    dangerouslyAllowBrowser: true,
+  });
+
+  const response = await openAI.chat.completions.create({
+    messages: [{ role: "user", content: prompt }],
+    model: "gpt-3.5-turbo",
+  });
+
+  const message = response.choices.at(0)?.message.content;
+  return message ? message : null;
 }
